f92f01897e2692ef035f47ff092597dda15a9bf8
cuisongliu <cuisongliu@qq.com>
2025-11-30 13:52:32 +0800

    Update INSTALL_skopeo.sh to extract and install skopeo without using containers-common

--- INSTALL_skopeo.sh
+++ INSTALL_skopeo.sh
@@ -3,9 +3,7 @@ set -e
 apt update -y \
   &&  apt install -y uidmap fuse-overlayfs curl wget
 wget http://ftp.gwdg.de/pub/opensuse/repositories/home:/alvistack/xUbuntu_22.04/amd64/skopeo_1.20.0-1_amd64.deb
-wget http://ftp.gwdg.de/pub/opensuse/repositories/home:/alvistack/xUbuntu_22.04/amd64/containers-common_0.64.2-1_amd64.deb
-apt install oci-runtime
-apt --fix-broken install
-dpkg -i containers-common_0.64.2-1_amd64.deb
-dpkg -i skopeo_1.20.0-1_amd64.de
+dpkg -X skopeo_1.20.0-1_amd64.deb  extract
+cp extract/usr/bin/skopeo /usr/local/bin/skopeo
+rm -rf skopeo_1.20.0-1_amd64.deb
 skopeo --version
