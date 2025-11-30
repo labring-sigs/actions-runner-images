52b2c573e3de1ab4095e28006a7c56945d5d5d0c
cuisongliu <cuisongliu@qq.com>
2025-11-30 11:17:17 +0800

    Update INSTALL_skopeo.sh to install sealos and streamline package management

--- INSTALL_skopeo.sh
+++ INSTALL_skopeo.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
-apt purge buildah golang-github-containers-common podman skopeo -y
-apt autoremove --purge  -y
-sh -c "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list"
+sudo apt update -y \
+  && sudo apt install -y uidmap fuse-overlayfs podman
+URL="https://github.com/labring/sealos/releases/download/v5.1.1/sealos_5.1.1_linux_amd64.tar.gz"
+curl -sfL $URL | sudo tar zx -C /usr/local/bin/ sealos
+sudo chmod a+x /usr/local/bin/sealos
+sudo sealos version
+sudo apt purge buildah golang-github-containers-common podman skopeo -y
+sudo apt autoremove --purge  -y
+sudo sh -c "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list"
 curl -fsSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/Release.key -o Release.key
-apt-key add Release.key
-apt-get update -y
-apt-get install skopeo uidmap fuse-overlayfs curl -y
-skopeo --version
+sudo apt-key add Release.key
+sudo apt update -y
+sudo apt-get install skopeo -y
 rm -f Release.key
