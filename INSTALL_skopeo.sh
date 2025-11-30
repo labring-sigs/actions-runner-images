5c77d23150e6362833a63f1bde15bd9b8dad4e35
cuisongliu <cuisongliu@qq.com>
2025-11-30 11:10:15 +0800

    Update installation script for skopeo to include necessary packages and remove obsolete ones

--- INSTALL_skopeo.sh
+++ INSTALL_skopeo.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
-apt install -y uidmap fuse-overlayfs curl
 apt purge buildah golang-github-containers-common podman skopeo -y
 apt autoremove --purge  -y
 sh -c "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list"
 curl -fsSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/Release.key -o Release.key
 apt-key add Release.key
 apt-get update -y
-apt-get install skopeo -y
+apt-get install skopeo uidmap fuse-overlayfs curl -y
 skopeo --version
 rm -f Release.key
