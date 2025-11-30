4b8b7c767eb9512cf5a97d1cc9da0863ab53627c
cuisongliu <cuisongliu@qq.com>
2025-11-30 11:19:17 +0800

    Update INSTALL_skopeo.sh to remove sudo and streamline package installation

--- INSTALL_skopeo.sh
+++ INSTALL_skopeo.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -e
-sudo apt update -y \
-  && sudo apt install -y uidmap fuse-overlayfs podman
-URL="https://github.com/labring/sealos/releases/download/v5.1.1/sealos_5.1.1_linux_amd64.tar.gz"
-curl -sfL $URL | sudo tar zx -C /usr/local/bin/ sealos
-sudo chmod a+x /usr/local/bin/sealos
-sudo sealos version
-sudo apt purge buildah golang-github-containers-common podman skopeo -y
-sudo apt autoremove --purge  -y
-sudo sh -c "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list"
+apt update -y \
+  &&  apt install -y uidmap fuse-overlayfs podman
+apt purge buildah golang-github-containers-common podman skopeo -y
+apt autoremove --purge  -y
+sh -c "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list"
 curl -fsSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/Release.key -o Release.key
-sudo apt-key add Release.key
-sudo apt update -y
-sudo apt-get install skopeo -y
+apt-key add Release.key
+apt update -y
+apt-get install skopeo -y
 rm -f Release.key
