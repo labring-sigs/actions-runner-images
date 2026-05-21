30d6ed48f799fbd1a52948e934ae861dca39f5be
cuisongliu <cuisongliu@qq.com>
2026-05-21 18:11:30 +0800

    fix: install skopeo from apt repository

--- INSTALL_skopeo.sh
+++ INSTALL_skopeo.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 apt update -y \
-  &&  apt install -y uidmap fuse-overlayfs curl wget
-wget -q https://ftp.gwdg.de/pub/opensuse/repositories/home:/alvistack/xUbuntu_22.04/amd64/skopeo_1.22.0-1_amd64.deb
-dpkg -X skopeo_1.22.0-1_amd64.deb  extract
-cp extract/usr/bin/skopeo /usr/local/bin/skopeo
-rm -rf skopeo_1.20.0-1_amd64.deb
-rm -rf extract
+  && apt install -y uidmap fuse-overlayfs curl wget skopeo
 skopeo --version
 
 curl https://gosspublic.alicdn.com/ossutil/install.sh | bash
